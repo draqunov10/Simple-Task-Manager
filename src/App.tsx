a4c5031e3a112494fbd231a074c8e82c5a233e66
Rhon <ledrhoniel@gmail.com>
2024-03-04 10:33:40 +0800

    small change

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import TaskCard from "./components/TaskCard";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <TaskCard />
+    </>
+  );
 }
 
 export default App;
